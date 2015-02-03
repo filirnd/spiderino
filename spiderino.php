480b4e26cef5b9c5951081dd72c9caef1bfef187
Flyer-90 <cantarella.danilo@gmail.com>
2015-02-03 08:42:51 +0100

    Update spiderino

--- spiderino.php
+++ spiderino.php
@@ -1,5 +1,13 @@
 <?php
 
+/* 
+sudo chmod -R 777 /var/www
+
+nel regex dei link aggiungere l'https 
+sempre nel regex se il link inizia con lo / allora bisogna appendere l'url all'inizio
+
+
+*/
 
 $url_queue = array();
 
@@ -12,7 +20,11 @@ if(isset($_SERVER["argv"][2])) {
 
 }
 
+if (!is_dir('output')) {
+    mkdir('output', 0777, true);
+}
 
+$nURL = 0;
 
 /*read first seed url and add it in queue*/
 /*echo "URL SEED ".$start_seed." \n";
@@ -38,16 +50,26 @@ while($index < count($url_queue)-1) {
 /* function that keep urls in &$queue from a page $siteUrl*/
 
 function readUrls($siteUrl,&$queue){
+	
+	global $nURL;
+	$siteUrl = clean_url($siteUrl);
+	if(check_file_ext($siteUrl) == 0)
+		return 0;
 	echo "Try getting url: ".$siteUrl."\n";
     $result = file_get_contents($siteUrl); /* download page */
    
-    /*check if page is empty*/
+    /*check if page is not empty*/
     if( $result )
     {
-        
+        $myfile = fopen("output/".$nURL.".txt", "a") or die("Unable to open file!");        	
+		fwrite($myfile, $result);
+		fclose($myfile);
+		$nURL++;
         echo "Start Parsing url: ".$siteUrl."\n";
         //echo "QUIII! \n";
         
+        //echo "\n*** ".check_file_ext($siteUrl)."\n";
+
         /* check if there is url in siteUrl */
         preg_match_all( '/<a.+?href="(http:\/\/[^0-9].+?)"/', $result, $urlmatch, PREG_SET_ORDER );
 		
@@ -67,7 +89,7 @@ function readUrls($siteUrl,&$queue){
         foreach( $urlmatch as $item )
         {
 
-            print_r($item[1]. " \n");
+            print_r("Found > " .$item[1]. " \n");
             /*if (!in_array($item[1], $queue)) { 
 					//echo ("item ".$item[1]." not in array.\n");
 					$file_ext="";
@@ -96,10 +118,48 @@ function readUrls($siteUrl,&$queue){
     }
 }
 
-/*
-function check_file_ext($url,$toReturn){
-		;
-}*/
+function clean_url($url){
+	if (substr($url, 0, 11) === 'http://www.')	
+		$url = "http://".substr($url, 11);
+
+	else if (substr($url, 0, 4) === 'www.')
+		$url = "http://".substr($url, 4);
+
+	else if (substr($url, 0, 12) === 'https://www.')
+		$url = "http://".substr($url, 12);
+	
+	else if(substr($url, 0, 7) !== 'http://')	
+		$url = "http://".$url;
+
+	//usare mb_substr?
+	if(substr($url, -1) === '/')
+		$url = substr($url, 0, -1);
+		//$url = $url."/";
+	return $url;
+}
+
+function check_file_ext($url){
+	
+	/*$url_arr = explode(".",$url);
+	$arrLength = count($url_arr);
+	$lastEle = $arrLength - 1;
+	$fileExt = $url_arr[$arrLength - 1]; //Gives the file extension
+	unset($url_arr[$lastEle]);
+	$urlMinusExt = implode(".",$url_arr);
+	return $fileExt;*/
+	if (strpos(substr($url, 7),'/') === false) { /*check if is domain homepage*/
+    	return 1;
+	}
+
+	$ext = substr(strrchr($url, "."), 1);
+	if(($ext === "html") || ($ext === "htm") || ($ext === "xhtml") || ($ext === "xml") || ($ext === "php")
+	    || ($ext === "txt") || ($ext === "asp") || ($ext === "aspx") || ($ext === "jsp") || ($ext === "jspx"))
+			return 1;
+	if (strpos($ext,'/') !== false) {		/*Case of folder*/
+    	return 1;		
+	}
+	return 0; 		/*Link not supported*/
+}
 
 
 ?>
