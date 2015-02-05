c067e97fc6cddeeaa9161cdfc4192b65ff548ef4
Flyer-90 <cantarella.danilo@gmail.com>
2015-02-05 01:40:31 +0100

    Add database to clean repeated urls

--- libs/database.php
+++ libs/database.php
@@ -9,20 +9,20 @@ class Database{
 
 
 	public static function createDb(){ /* create db if not exist */
-		echo "---- Try to create DB ". self::$dbName ."\n";
+		//echo "---- Try to create DB ". self::$dbName ."\n";
 		// Create connection
 		$conn = new mysqli( self::$servername, self::$username, self::$password);
 		// Check connection
 		if ($conn->connect_error) {
-			die("---- CreateDB connection failed: " . $conn->connect_error);
+			die("CreateDB connection failed: " . $conn->connect_error);
 		} 
 
 		// Create database
 		$sql = "CREATE DATABASE ". self::$dbName;
 		if ($conn->query($sql) === TRUE) {
-			echo "---- Database created successfully\n";
+			echo "Database created successfully\n";
 		} else {
-			echo "---- Error creating database: " . $conn->error. "\n";
+			echo "Error creating database: " . $conn->error. "\n";
 		}
 
 		$conn->close();
@@ -30,12 +30,12 @@ class Database{
 
 
 	public static function createTable(){
-		echo "---- Try to create table ". self::$tableName ."\n";
+		//echo "Try to create table ". self::$tableName ."\n";
 		// Create connection
 		$conn = mysqli_connect(self::$servername, self::$username, self::$password, self::$dbName);
 		// Check connection
 		if (!$conn) {
-			die("---- CreateTable connection failed: " . mysqli_connect_error());
+			die("CreateTable connection failed: " . mysqli_connect_error());
 		}
 
 
@@ -43,9 +43,9 @@ class Database{
 		$sql = "CREATE TABLE ".self::$tableName." (id INT(11) UNSIGNED , url VARCHAR(300) NOT NULL PRIMARY KEY, father VARCHAR(300) NOT NULL, depth INT(11) UNSIGNED NOT NULL, discoveredurls INT(11) UNSIGNED NOT NULL)";
 
 		if (mysqli_query($conn, $sql)) {
-			echo "---- Table ".self::$tableName." created successfully \n";
+			echo "Table ".self::$tableName." created successfully \n\n";
 		} else {
-			echo "---- Error creating table: " . mysqli_error($conn)."\n";
+			echo "Error creating table: " . mysqli_error($conn)."\n\n";
 		}
 
 		mysqli_close($conn);
@@ -60,17 +60,17 @@ class Database{
 		$conn = mysqli_connect(self::$servername, self::$username, self::$password, self::$dbName);
 		// Check connection
 		if (!$conn) {
-			die("---- Insert function connection failed: " . mysqli_connect_error()). "\n";
+			die("Insert function connection failed: " . mysqli_connect_error()). "\n";
 		}
 
-		echo $sql = "INSERT INTO ".self::$tableName." (id, url, father, depth, discoveredurls) VALUES ('".$id."', '".$url."', '".$father."', '".$depth."', '".$discoveredurls."')";
+		$sql = "INSERT INTO ".self::$tableName." (id, url, father, depth, discoveredurls) VALUES ('".$id."', '".$url."', '".$father."', '".$depth."', '".$discoveredurls."')";
 
 		if (mysqli_query($conn, $sql)) {
-			echo "---- New record created successfully \n";
+			echo $url." inserted successfully.\n";
 
 		} else {
 			$toReturn = 1;
-			echo "---- Error: " . $sql . "<br>" . mysqli_error($conn) . "\n";
+			//echo "\n---- Error: " . $sql . "\n" . mysqli_error($conn) . "\n";
 		}
 
 		mysqli_close($conn);
@@ -78,6 +78,33 @@ class Database{
 
 
 	}
+
+	/* return 0 if record is correctly update, 1 otherwise */
+	public static function update($id, $url, $discoveredurls){
+		$toReturn = 0;
+		// Create connection
+		$conn = mysqli_connect(self::$servername, self::$username, self::$password, self::$dbName);
+		// Check connection
+		if (!$conn) {
+			die("Update function connection failed: " . mysqli_connect_error()). "\n";
+		}
+
+		$sql = "UPDATE ".self::$tableName." SET id = '".$id."', discoveredurls = '".$discoveredurls."' WHERE url = '".$url."' ";
+
+		if (mysqli_query($conn, $sql)) {
+			echo $url." updated successfully.\n";
+
+		} else {
+			$toReturn = 1;
+			//echo "---- Error: " . $sql . "<br>" . mysqli_error($conn) . "\n";
+		}
+
+		mysqli_close($conn);
+		return $toReturn;
+
+
+	}
+
 }
 
 ?>
--- spiderino.php
+++ spiderino.php
@@ -2,20 +2,13 @@
 
 /* sudo chmod -R 777 /var/www
 
--nel regex dei link aggiungere l'https (gia fatto?!)
--sempre nel regex se il link inizia con lo / allora bisogna appendere l'url all'inizio
 -implementare tempo di processamento max
--ripetizioni con db
--contare quanti url sono trovati nella pagina.
--prima di parsare un url provare a inserirlo nel db, se la funz insert() ritorna 0 parsare, altrimenti era gia stato processato.
 -calcolo profondità
--nel db id (indice del file), url, father(pagina dove è stato trovato url, per costruire il grafo), depth (profondità),
-discoveredurls (numero urls trovati nella pagina)*/
-
-
-
+-stampare usage se si passano parametri sbagliati */
 
 require ("libs/database.php");
+
+ini_set('memory_limit', '1024M');
 
 /*init database */
 Database::createDb();
@@ -53,33 +46,44 @@ if (!is_dir('output')) {
 	mkdir('output', 0777, true);
 }
 
-$nURL = 0;
+$idURL = 1;
 
 /*read first seed url and add it in queue*/
 /*echo "URL SEED ".$start_seed." \n";
 echo "QUEU".count($url_queue)."\n";
 */
+
+/*Clean seed url*/
+$start_seed = clean_url($start_seed);
+	if(check_file_ext($start_seed) == 0) /*Check if url is valid*/
+		return 0;
+
+/*Query to insert seed url in DB*/
+$fromDb = Database::insert(NULL, $start_seed, "Initial seed Url", NULL, NULL); 
+
+/*Parse seed url to find new Urls*/
 readUrls($start_seed,$url_queue);
 
-$index=0;
+$index = 0;
 
 /* Loop readPage while there is element in queue */
-while($index < count($url_queue)-1) {
-	echo ("-----WHILE ENTER WITH INDEX:".$index."\n");
+while($index < count($url_queue)) {
+	//echo ("-----WHILE ENTER WITH INDEX:".$index."\n");
 	readUrls($url_queue[$index],$url_queue);
 	$index++;
-	sleep($delay);
+	//sleep($delay);
 
 }
 
 /* function that keep urls in &$queue from a page $siteUrl*/
 
-function readUrls($siteUrl,&$queue){
+function readUrls($siteUrl, &$queue){
 	
-	global $nURL, $key, $argc, $argv;
-	$siteUrl = clean_url($siteUrl);
-	if(check_file_ext($siteUrl) == 0)
-		return 0;
+	global $idURL, $key, $argc, $argv;
+	//$siteUrl = clean_url($siteUrl);
+	//if(check_file_ext($siteUrl) == 0) /*Check if url is valid*/
+	//	return 0;
+
 	echo "Try getting url: ".$siteUrl."\n";
 	$result = file_get_contents($siteUrl); /* download page */
 
@@ -87,62 +91,28 @@ function readUrls($siteUrl,&$queue){
 	if( $result )
 	{
 		echo "Start Parsing url: ".$siteUrl."\n";
-        //echo "QUIII! \n";
-
-        //echo "\n*** ".check_file_ext($siteUrl)."\n";
-
+      	
 		/* check if there is url in siteUrl */
 		preg_match_all( '/<a.+?href="((http:\/\/|https:\/\/|\/|)[a-zA-Z0-9].+?)"/', $result, $urlmatch, PREG_SET_ORDER );
-		$valid = 0;
 
-		/* check if searched words are in page*/
-		$found = preg_match_all( '/'.$key.'/i', $result, $words, PREG_SET_ORDER );
-
-		if($found > 0){
-			echo "word ".$key. " is in page ".$siteUrl."\n";
-			if($argc == 3) 
-				$valid = 1;
-			for($i = 3; $i < $argc; $i++) {
-				echo "search word ".$argv[$i]."\n";
-				$found = preg_match_all( '/'.$argv[$i].'/i', $result, $words, PREG_SET_ORDER );
-				if($found > 0) {
-					$valid = 1;
-					echo "word ".$argv[$i]. " is in page ".$siteUrl."\n";
-					break;
-				}
-			}
-
-			if($valid == 1) {
-
-				$myfile = fopen("output/".$nURL.".txt", "w") or die("Unable to open file!");        	
-				fwrite($myfile, $result);
-				fclose($myfile);
-				$nURL++;
-
-				$myfile = fopen("pagewithword.txt", "a") or die("Unable to open file!");        	
-				$txt = "word mail is in page ".$siteUrl."\n";
-				fwrite($myfile, $txt);
-				fclose($myfile);
-			}
-
-		}
-
+		/*Extract domain url (actual folder)*/
 		$domain = $siteUrl;
 		$pos = strrpos( substr($siteUrl, 7), '/');
 		if ($pos !== false) {
 			$domain = substr($siteUrl, 0, 7 + $pos);
 		}
-		echo ">>dominio  ".$domain."\n";
+		//echo ">> dominio  ".$domain."\n";
 
+		/*Extract main domain url*/
 		$firstDomain = substr($siteUrl,7);
 		$pos = strstr($firstDomain, '/', true);
 		if ($pos !== false) {
 			$firstDomain = "http://".$pos;
-			echo ">>dominio first  ".$firstDomain."\n";
+			//echo ">> dominio first  ".$firstDomain."\n";
 		}
 		//$firstDomain = "http://".strstr($firstDomain, '/');
 
-		
+		$nURLFounded = 0;
 
 		foreach( $urlmatch as $item ) /*Add founded urls in queue*/
 		{
@@ -159,22 +129,82 @@ function readUrls($siteUrl,&$queue){
             
             $tempUrl = clean_url($tempUrl);
             if(check_file_ext($tempUrl) == 1) { 
-            	print_r("**Valid > " .$tempUrl. " \n");
-            	array_push($queue,$tempUrl);
+
+	            /*Query to insert url in DB*/
+	            $fromDb = Database::insert(NULL, $tempUrl, $siteUrl, NULL, NULL); 
+				if ($fromDb == 0){
+					//print_r("Valid  " .$tempUrl. " \n");
+	            	array_push($queue,$tempUrl);
+					$nURLFounded++;
+					echo "Dimensione coda ".count($queue). "\n";
+
+				} else {
+					echo "Url ".$tempUrl." ripetuto. \n";
+				}	
+
+            	
             }
         }
 
-        echo "Finish Parsing url: ".$siteUrl."\n";
+        $valid = 0;
+        $actualIdFile = 0;
 
-    } else {
-    	/*append url that not working in a file*/
+		/* check if searched words are in page*/
+		$found = preg_match_all( '/'.$key.'/i', $result, $words, PREG_SET_ORDER );
+
+		if($found > 0){ /*If first keyword founded*/
+			echo "Word ".$key. " is in page ".$siteUrl."\n";
+			if($argc == 3)  /*Case if there is only one keyword*/
+				$valid = 1;
+			for($i = 3; $i < $argc; $i++) { /*check if there is almost one another keyword*/
+				//echo "search word ".$argv[$i]."\n";
+				$found = preg_match_all( '/'.$argv[$i].'/i', $result, $words, PREG_SET_ORDER );
+				if($found > 0) {
+					$valid = 1;
+					echo "Word ".$argv[$i]. " is in page ".$siteUrl."\n";
+					break;
+				}
+			}
+
+			if($valid == 1) { /*If in this page there are keywords*/
+
+				/*Write a file with idURL as name*/
+				$myfile = fopen("output/".$idURL.".txt", "w") or die("Unable to open file!");        	
+				fwrite($myfile, $result);
+				fclose($myfile);
+				$actualIdFile = $idURL;
+				$idURL++;
+
+				//$myfile = fopen("pagewithword.txt", "a") or die("Unable to open file!");        	
+				//$txt = "word mail is in page ".$siteUrl."\n";
+				//fwrite($myfile, $txt);
+				//fclose($myfile);
+			}
+
+		}
+
+		
+		if($nURLFounded != 0 || $actualIdFile !=0) {
+			/*Query to update url's info in DB*/
+			$fromDb = Database::update($actualIdFile, $siteUrl, $nURLFounded);
+			//if ($fromDb == 0){
+			//	print_r("*** Update > " .$siteUrl. " \n");
+
+			//}
+		}
+		
+		echo "Finish Parsing url: ".$siteUrl."\n\n";
+
+    } 
+    /*else {
+    	/*append url that not working in a file
     	$myfile = fopen("notworkingurls.txt", "a") or die("Unable to open file!");        	
     	$txt = "not work ".$siteUrl."\n";
     	fwrite($myfile, $txt);
     	fclose($myfile);
     	
     	
-    }
+    }*/
 }
 
 function clean_url($url){
